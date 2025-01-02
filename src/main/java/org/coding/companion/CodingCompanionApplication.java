300ee1fca18e049470493f9569a74fc347d38c5f
Bimales Mandal <mail2bimales@gmail.com>
2025-01-02 15:50:43 +0530

    base package for coding-companion

---
+++ src/main/java/org/coding/companion/CodingCompanionApplication.java
@@ -0,0 +1,13 @@
+package org.coding.companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/coding/companion/CodingCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coding.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
